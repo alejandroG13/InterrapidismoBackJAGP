4b3aef0b5c901396a22a4309225d3b0f2cf41554
alejandroG13 <58273099+alejandroG13@users.noreply.github.com>
2026-02-01 22:05:37 -0500

    Create DML_BD_UNIVERSIDAD.sql

---
+++ DML_BD_UNIVERSIDAD.sql
@@ -0,0 +1,22 @@
+INSERT INTO profesores (nombre) VALUES
+('Profesora Andrea'),
+('Profesor Bruno'),
+('Profesor Carlos'),
+('Profesor Daniel'),
+('Profesor Elias');
+
+INSERT INTO materias (nombre, id_profesor) VALUES
+('Matemáticas I', 1),
+('Matemáticas II', 1),
+('Física I', 2),
+('Física II', 2),
+('Programación I', 3),
+('Programación II', 3),
+('Bases de Datos', 4),
+('Redes', 4),
+('Ingeniería de Software', 5),
+('Sistemas Operativos', 5);
+
+
+insert into estudiantes (nombres, apellidos, email, password) values 
+('Jhon', 'Doe Garica', 'a@a.com', '123124');
